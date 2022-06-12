aca4c21badff29ec60ccf800d8eced261612312d
JungGyeongmi <gm950715@gmail.com>
2022-06-12 14:36:02 +0900

    변수연습문제 업로드

---
+++ Variable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Int32;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요");
+            String width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 너비를 입력하세요");
+            String height = Console.ReadLine();
+
+            int result = Parse(width) * Parse(height);
+
+            Console.WriteLine($"사각형의 너비는 {result} 입니다");
+        }
+    }
+}
